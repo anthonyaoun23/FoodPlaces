8d9f481e31dd68d770537d05c00cdcaec6b0564c
Anthony <anthony.aoun1@hotmail.com>
2019-06-03 01:59:10 -0400

    added redux store to app

--- react-frontend/src/api/shop.js
+++ react-frontend/src/api/shop.js
@@ -1,11 +1,11 @@
 /**
  * Mocking client-server processing
  */
-import _products from './products.json'
+import _restaurants from './restaurants.json';
 
-const TIMEOUT = 100
+const TIMEOUT = 100;
 
 export default {
-  getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
+  getProducts: (cb, timeout) => setTimeout(() => cb(_restaurants), timeout || TIMEOUT),
   buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
 }
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Redux Stuff
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 import { getAllProducts } from './actions';
+
+
 import App from './App';
 import './style/App.css';
 
+const middleware = [ thunk ];
+if (ProcessingInstruction.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+);
+
+store.dispatch(getAllProducts());
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}><App/></Provider>,
   document.getElementById('root')
 );
