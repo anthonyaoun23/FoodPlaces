445af3ef785105a60e6ca2296aee56ff254c9152
Anthony <anthony.aoun1@hotmail.com>
2019-06-03 01:53:30 -0400

    adding redux store

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -12,6 +12,7 @@ import Payment from './components/pages/payment/Payment';
 import RestoList from './components/pages/resto-list/RestoList';
 import RestoMenu from './components/pages/resto-menu/RestoMenu';
 
+import Test from './components/pages/test/Test';
 
 
 class App extends Component {
@@ -24,6 +25,7 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path='/' component={Home} />
+            <Route exact path='/test' component={Test} />
             <Route exact path='/cart' component={Cart} />
             <Route exact path='/payment' component={Payment} />
             <Route exact path='/list' component={RestoList} />
