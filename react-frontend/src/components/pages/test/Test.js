5948a7548a36bb2f14fbc2c3b9e44886d788342f
Anthony <anthony.aoun1@hotmail.com>
2019-06-03 02:28:04 -0400

    Update Test.js

---
+++ react-frontend/src/components/pages/test/Test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import TestProductsContainer from './TestProductsContainer';
+import TestCartContainer from './TestCartContainer';
+
+const Test = () => {
+    <div>
+        <h2>Test add to cart and checkout functionality</h2>
+        <hr/>
+        <TestProductsContainer />
+        <hr/>
+        <TestCartContainer />
+    </div>
+}
+
+export default Test;
