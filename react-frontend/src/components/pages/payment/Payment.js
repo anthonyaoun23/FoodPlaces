4b4a99774a63fad15a3c881b8d8e3f9581f65e9e
Anthony <anthony.aoun1@hotmail.com>
2019-06-02 21:00:47 -0400

    made payment component

---
+++ react-frontend/src/components/pages/payment/Payment.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+
+export default class Payment extends Component {
+    render() {
+        return (
+            <div className="layout_paiment">
+                <div className="paiment">
+                    <div className="paiment__header">
+                        <h1>Checkout</h1>
+                    </div>
+
+                    <div className="paiment__address">
+                        <h1>Address Information</h1>
+                        <p>Is the address you'd like to use displayed below? If so, click the corresponding "Ship to this address" button. Or you can enter a new shipping address</p>
+
+                        <form action="">
+                            <div className="address-info">
+                                <label for="fullname">Full Name</label>
+                                <input id="fullname" type="text"/>
+                            </div>
+                            <div className="address-info">
+                                <label for="address">Address Line</label>
+                                <input id="address" type="text"/>
+                            </div>
+                            <div className="address-info">
+                                <label for="icity">City</label>
+                                <input id="icity" type="text"/>
+                            </div>
+                            <div className="address-info">
+                                <label for="province">Province</label>
+                                <input id="province" type="text"/>
+                            </div>
+                            <div className="address-info">
+                                <label for="postal">Postal Code</label>
+                                <input id="postal" type="text"/>
+                            </div>
+                            <div className="address-info">
+                                <label for="phone">Phone Number</label>
+                                <input id="phone" type="phone"/>
+                            </div>
+                        </form>
+
+                    </div>
+
+                    <div className="paiment__method">
+                        <h1>Payment Method</h1>
+                        <div className="payment-info">
+                            <p>FoodPlaces accepts all major credit and debit cards:</p> <i className="fab fa-cc-visa"></i> <i className="fab fa-cc-mastercard"></i> <i className="fab fa-cc-discover"></i> <i className="fab fa-cc-amex"></i>
+                        </div>
+
+                        <form action="">
+                            <div className="payment-details">
+                                <div className="cardnumber">
+                                    <label for="fullname">Card Number</label>
+                                    <input id="fullname" type="text"/>
+                                </div>
+                                <div className="cardnumber__details">
+                                    <div className="cardnumber-info cardnumber__details__name">
+                                        <label for="nameoncard">Name on Card</label>
+                                        <input id="nameoncard" type="text"/>
+                                    </div>
+                                    <div className="cardnumber-info cardnumber__details__exp">
+                                        <label for="icity">Expiry Date</label>
+                                        <input id="icity" type="text"/>
+                                    </div>
+                                    <div className="cardnumber-info cardnumber__details__cvv">
+                                        <label for="province">CVV Code</label>
+                                        <input id="province" type="text"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+            
+                    </div>
+
+                    <div className="paiment__checkout">
+                        <p className="checkout__summary">Order Summary</p>
+                        <div className="checkout__list">
+                            <div className="cart__item item">
+                                <div className="item__img img-container">
+                                    <img className="img-container__img" src="./thomas-tucker-391058-unsplash.jpg" alt="" />
+                                </div>
+                                <div className="item__info info">
+                                    <p className="info__name">Restaurant Name</p>
+                                    <h4 className="info__title">Order Item Name</h4>
+                                    <div className="information">
+                                        <div className="info__price">
+                                            <div className="price">
+                                                <p className="price__text">$30.00</p>
+                                            </div>
+                                        </div>
+                                        <button className="info__details">
+                                            <div className="details">
+                                                <p className="price__text2">Details<i className="fas fa-angle-down details__angle"></i></p>
+                                            </div>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="cart__item item">
+                                <div className="item__img img-container">
+                                    <img className="img-container__img" src="./thomas-tucker-391058-unsplash.jpg" alt="" />
+                                </div>
+                                <div className="item__info info">
+                                    <p className="info__name">Restaurant Name</p>
+                                    <h4 className="info__title">Order Item Name</h4>
+                                    <div className="information">
+                                        <div className="info__price">
+                                            <div className="price">
+                                                <p className="price__text">$30.00</p>
+                                            </div>
+                                        </div>
+                                        <button className="info__details">
+                                            <div className="details">
+                                                <p className="price__text2"> Details<i className="fas fa-angle-down details__angle"></i></p>
+                                            </div>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        </div>
+
+                        <div className="coupon">
+                            <form className="coupon__form" action="">
+                                <input placeholder="Add Coupon" type="text" />
+                                <button >Apply</button>
+                            </form>
+                        </div>
+                        <div className="pay">
+                            <button>PAY $60.00</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
