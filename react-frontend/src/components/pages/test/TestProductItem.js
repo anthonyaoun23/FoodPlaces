e541e8627051d9524974b519cb6093e7368f6ab5
Anthony <anthony.aoun1@hotmail.com>
2019-06-03 01:52:58 -0400

    Create TestProductItem.js

---
+++ react-frontend/src/components/pages/test/TestProductItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Product from './TestProduct';
+
+const TestProductItem = ({ product, onAddToCartClicked }) => (
+    <div style={{ marginBottom: 20 }}>
+        <Product title={product.title} price={product.price} quantity={product.inventory} />
+        <button onClick={onAddToCartClicked} disabled={product.inventory > 0 ? '' : 'disabled'}>{product.inventory > 0 ? 'Add to cart' : 'Sold Out'}</button>
+    </div>
+);
+
+TestProductItem.propTypes = {
+    product: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      inventory: PropTypes.number.isRequired
+    }).isRequired,
+    onAddToCartClicked: PropTypes.func.isRequired
+  }
+
+  export default TestProductItem;
