c12e61c88df7f3bf96d51cf7667a7f0729882359
Anthony <anthony.aoun1@hotmail.com>
2019-06-03 02:28:07 -0400

    Create TestCartContainer.js

---
+++ react-frontend/src/components/pages/test/TestCartContainer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { checkout } from '../../../actions';
+import { getTotal, getCartProducts } from '../../../reducers';
+
+import TestCart from './TestCart';
+
+const TestCartContainer = ({ products, total , checkout }) => (
+    <TestCart products={products} total={total} onCheckoutClicked={checkout(products)} />
+);
+
+TestCartContainer.propTypes = {
+    products: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      quantity: PropTypes.number.isRequired
+    })).isRequired,
+    total: PropTypes.string,
+    checkout: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    products: getCartProducts(state),
+    total: getTotal(state)
+});
+
+export default connect(
+    mapStateToProps,
+    { checkout }
+)(TestCartContainer);
