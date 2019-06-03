485d00e98af4557ee6d52f77a00d18e8c2fa160a
Anthony <anthony.aoun1@hotmail.com>
2019-06-03 01:53:12 -0400

    Update index.js

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import { getAllProducts } from './actions';
 import App from './App';
 import './style/App.css';
 
