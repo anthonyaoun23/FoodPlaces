0e5f341a11122566ada7ceae7302dea3cb00a6e8
Anthony <anthony.aoun1@hotmail.com>
2019-06-05 15:41:59 -0400

    fix home page

--- react-frontend/src/components/pages/home/Home.js
+++ react-frontend/src/components/pages/home/Home.js
@@ -1,66 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import bgImage from '../../../assets/thomas-tucker-391058-unsplash.jpg';
+import bgImage from "../../../assets/thomas-tucker-391058-unsplash.jpg";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div className="layout">
-            <div className="layout__header header">
-              <div className="header__pitch pitch">
-                <div className="pitch__container">
-                  <h1 className="pitch__maintext">Order food <br />the easy way</h1>
-                  <p className="pitch__subtext">Choose your favourite meals now.</p>
-                  <button className="pitch__button">Explore All</button>
-                </div>
-              </div>
-              <div className="header__image-container image-container">
-      
-                <img src={bgImage} alt="image of pizza" />
-              </div>
-            </div>
-            <div className="search-container">
-              <form className="header__search search">
-                <div className="search__search styled-input">
-                  <input className="border-left" placeholder="Search" type="text" />
-                  <i className="fas fa-search"></i>
-                </div>
-                <div className="search__category styled-input">
-                  {/* <!-- <input type="tel" placeholder="Italian & Pizza" required /> --> */}
-                  <select className="category-select">
-                    <option className="disabled" disabled defaultChecked value="books"
-                      >Select Food Category</option
-                    >
-                    <option value="books">Italian & Pizza</option>
-                    <option value="html">Japanese & Sushi</option>
-                    <option value="css">Coffee & Tea</option>
-                    <option value="php">Noodle Soups</option>
-                    <option value="js">Rice</option>
-                  </select>
-                  <i className="fas fa-angle-down"></i>
-                </div>
-                <div className="search__city styled-input">
-                  <select className="city-select">
-                    <option className="disabled" disabled defaultChecked value="books"
-                      >Select City</option
-                    >
-                    <option value="books">Ottawa</option>
-                    <option value="html">Montreal</option>
-                    <option value="css">Toronto</option>
-                    <option value="php">Waterloo</option>
-                    <option value="js">Kingston</option>
-                    <option value="js">Hamilton</option>
-                  </select>
-                  <i className="fas fa-ellipsis-h"></i>
-                </div>
-                <div className="search__button styled-input">
-                  <button className="border-right search-button" type="button">
-                    Search
-                  </button>
-                </div>
-              </form>
+  render() {
+    return (
+      <div className="layout">
+        <div className="layout__header header">
+          <div className="header__pitch pitch">
+            <div className="pitch__container">
+              <h1 className="pitch__maintext">
+                Order food <br />
+                the easy way
+              </h1>
+              <p className="pitch__subtext">Choose your favourite meals now.</p>
+              <button className="pitch__button">Explore All</button>
             </div>
           </div>
-        );
-    }
+          <div className="header__image-container image-container">
+            <img src={bgImage} alt="image of pizza" />
+          </div>
+        </div>
+        <div className="search-container">
+          <form className="header__search search">
+            <div className="search__search styled-input">
+              <input className="border-left" placeholder="Search" type="text" />
+              <i className="fas fa-search" />
+            </div>
+            <div className="search__category styled-input">
+              {/* <!-- <input type="tel" placeholder="Italian & Pizza" required /> --> */}
+              <select className="category-select">
+                <option
+                  className="disabled"
+                  disabled
+                  defaultChecked
+                  value="books"
+                >
+                  Select Food Category
+                </option>
+                <option value="books">Italian & Pizza</option>
+                <option value="html">Japanese & Sushi</option>
+                <option value="css">Coffee & Tea</option>
+                <option value="php">Noodle Soups</option>
+                <option value="js">Rice</option>
+              </select>
+              <i className="fas fa-angle-down" />
+            </div>
+            <div className="search__city styled-input">
+              <select className="city-select">
+                <option
+                  className="disabled"
+                  disabled
+                  defaultChecked
+                  value="books"
+                >
+                  Select City
+                </option>
+                <option value="books">Ottawa</option>
+                <option value="html">Montreal</option>
+                <option value="css">Toronto</option>
+                <option value="php">Waterloo</option>
+                <option value="js">Kingston</option>
+                <option value="js">Hamilton</option>
+              </select>
+              <i className="fas fa-ellipsis-h" />
+            </div>
+            <div className="search__button styled-input">
+              <button className="border-right search-button" type="button">
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
