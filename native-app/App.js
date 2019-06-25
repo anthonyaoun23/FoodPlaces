97498710135b4178def4bd866bd8c2af59378cd8
Anthony <anthony.aoun1@hotmail.com>
2019-06-24 23:12:25 -0400

    started home page

--- native-app/App.js
+++ native-app/App.js
@@ -1,82 +1,141 @@
-import React from 'react';  
-import {StyleSheet, Text, View,Button} from 'react-native';  
-import { createBottomTabNavigator, createAppContainer} from 'react-navigation';  
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';  
-import Icon from 'react-native-vector-icons/Ionicons';  
-class HomeScreen extends React.Component {  
-  render() {  
-    return (  
-        <View style={styles.container}>  
-          <Text>Home Screen</Text>  
-        </View>  
-    );  
-  }  
-}  
-class MenuScreen extends React.Component {  
-  render() {  
-    return (  
-        <View style={styles.container}>  
-          <Text>Menu Screen</Text>  
-        </View>  
-    );  
-  }  
-}   
-class CartScreen extends React.Component {  
-    render() {  
-        return (  
-            <View style={styles.container}>  
-                <Text>Cart Screen</Text>  
-            </View>  
-        );  
-    }  
-}  
-const styles = StyleSheet.create({  
-    container: {  
-        flex: 1,  
-        justifyContent: 'center',  
-        alignItems: 'center'  
-    },  
-});  
-const TabNavigator = createMaterialBottomTabNavigator(  
-    {  
-        Home: { screen: HomeScreen,  
-            navigationOptions:{  
-                tabBarLabel:'Home',  
-                tabBarIcon: ({ tintColor }) => (  
-                    <View>  
-                        <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>  
-                    </View>),  
-            }  
-        },  
-        Menu: { screen: MenuScreen,  
-            navigationOptions:{  
-                tabBarLabel:'Menu',  
-                tabBarIcon: ({ tintColor }) => (  
-                    <View>  
-                        <Icon style={[{color: tintColor}]} size={25} name={'ios-list'}/>  
-                    </View>),  
-                 activeColor: '#615af6',  
-                 inactiveColor: '#46f6d7',  
-                 barStyle: { backgroundColor: '#67baf6' },
-            }  
-        },   
-        Cart: {  
-            screen: CartScreen,  
-            navigationOptions:{  
-                tabBarLabel:'Cart',  
-                tabBarIcon: ({ tintColor }) => (  
-                    <View>  
-                        <Icon style={[{color: tintColor}]} size={25} name={'ios-cart'}/>  
-                    </View>),  
-            }  
-        },  
-    },  
-    {  
-      initialRouteName: "Home",  
-      activeColor: '#f0edf6',  
-      inactiveColor: '#226557',  
-      barStyle: { backgroundColor: '#3BAD87' },  
-    },  
-);  
-  
-export default createAppContainer(TabNavigator);  
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { Searchbar, RadioButton } from "react-native-paper";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
+import RadioGroup from 'react-native-radio-buttons-group';
+
+class HomeScreen extends React.Component {
+  state = {
+    firstQuery: "", clicked: false, checked: 'first', data: [{label: 'Pizza'}, {label: 'Fish'}, {label: "Sushi"}, {label:'Money'}]
+  };
+
+  onPress = data => this.setState({data});
+
+  render() {
+    let selectedButton = this.state.data.find(e => e.selected == true);
+    selectedButton = selectedButton ? selectedButton.value : this.state.data[0].label;
+    const { firstQuery, clicked, checked } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <Searchbar
+          style={{ marginTop: 60 }}
+          placeholder="Search"
+          onChangeText={query => {
+            this.setState({ firstQuery: query });
+          }}
+          value={firstQuery}
+        />
+        <Text>Select Food Type</Text>
+        <Text>Currently Selected: {selectedButton}</Text>
+
+
+        <RadioGroup radioButtons={this.state.data} onPress={this.onPress} />
+      </View>
+    );
+  }
+}
+
+class FoodTypeGroup extends React.Component {
+  state = {
+    value: 'first',
+  };
+
+  render() {
+    return(
+      <RadioButton.Group
+        onValueChange={value => this.setState({ value })}
+        value={this.state.value}
+      >
+        <View>
+          <Text>First</Text>
+          <RadioButton value="first" />
+        </View>
+        <View>
+          <Text>Second</Text>
+          <RadioButton value="second" />
+        </View>
+      </RadioButton.Group>
+    )
+  }
+}
+
+class RestaurantsScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Restaurants Screen</Text>
+      </View>
+    );
+  }
+}
+class CartScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Cart Screen</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{ color: tintColor }]} size={25} name={"ios-home"} />
+          </View>
+        )
+      }
+    },
+    Restaurants: {
+      screen: RestaurantsScreen,
+      navigationOptions: {
+        tabBarLabel: "Restaurants",
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon
+              style={[{ color: tintColor }]}
+              size={25}
+              name={"ios-restaurant"}
+            />
+          </View>
+        ),
+        activeColor: "#615af6",
+        inactiveColor: "#46f6d7",
+        barStyle: { backgroundColor: "#67baf6" }
+      }
+    },
+    Cart: {
+      screen: CartScreen,
+      navigationOptions: {
+        tabBarLabel: "Cart",
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{ color: tintColor }]} size={25} name={"ios-cart"} />
+          </View>
+        )
+      }
+    }
+  },
+  {
+    initialRouteName: "Home",
+    activeColor: "#f0edf6",
+    inactiveColor: "#226557",
+    barStyle: { backgroundColor: "#3BAD87" }
+  }
+);
+
+export default createAppContainer(TabNavigator);
