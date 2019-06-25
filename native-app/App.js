0d1230c48014c4cbc6ca1e55da8259380abacf6d
Anthony <anthony.aoun1@hotmail.com>
2019-06-24 22:32:05 -0400

    added bottom tab navigation

--- native-app/App.js
+++ native-app/App.js
@@ -1,19 +1,82 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>APP PAGE</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import React from 'react';  
+import {StyleSheet, Text, View,Button} from 'react-native';  
+import { createBottomTabNavigator, createAppContainer} from 'react-navigation';  
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';  
+import Icon from 'react-native-vector-icons/Ionicons';  
+class HomeScreen extends React.Component {  
+  render() {  
+    return (  
+        <View style={styles.container}>  
+          <Text>Home Screen</Text>  
+        </View>  
+    );  
+  }  
+}  
+class MenuScreen extends React.Component {  
+  render() {  
+    return (  
+        <View style={styles.container}>  
+          <Text>Menu Screen</Text>  
+        </View>  
+    );  
+  }  
+}   
+class CartScreen extends React.Component {  
+    render() {  
+        return (  
+            <View style={styles.container}>  
+                <Text>Cart Screen</Text>  
+            </View>  
+        );  
+    }  
+}  
+const styles = StyleSheet.create({  
+    container: {  
+        flex: 1,  
+        justifyContent: 'center',  
+        alignItems: 'center'  
+    },  
+});  
+const TabNavigator = createMaterialBottomTabNavigator(  
+    {  
+        Home: { screen: HomeScreen,  
+            navigationOptions:{  
+                tabBarLabel:'Home',  
+                tabBarIcon: ({ tintColor }) => (  
+                    <View>  
+                        <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>  
+                    </View>),  
+            }  
+        },  
+        Menu: { screen: MenuScreen,  
+            navigationOptions:{  
+                tabBarLabel:'Menu',  
+                tabBarIcon: ({ tintColor }) => (  
+                    <View>  
+                        <Icon style={[{color: tintColor}]} size={25} name={'ios-list'}/>  
+                    </View>),  
+                 activeColor: '#615af6',  
+                 inactiveColor: '#46f6d7',  
+                 barStyle: { backgroundColor: '#67baf6' },
+            }  
+        },   
+        Cart: {  
+            screen: CartScreen,  
+            navigationOptions:{  
+                tabBarLabel:'Cart',  
+                tabBarIcon: ({ tintColor }) => (  
+                    <View>  
+                        <Icon style={[{color: tintColor}]} size={25} name={'ios-cart'}/>  
+                    </View>),  
+            }  
+        },  
+    },  
+    {  
+      initialRouteName: "Home",  
+      activeColor: '#f0edf6',  
+      inactiveColor: '#226557',  
+      barStyle: { backgroundColor: '#3BAD87' },  
+    },  
+);  
+  
+export default createAppContainer(TabNavigator);  
