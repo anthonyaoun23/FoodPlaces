5fb3081403b8f9320b47f2062bd9140073bc9267
Cedric Brisebois <ced2142b@gmail.com>
2019-06-26 00:44:38 -0400

    implemented restaurant list

--- native-app/components/screens/resto-list/RestoList.js
+++ native-app/components/screens/resto-list/RestoList.js
@@ -1,19 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, FlatList, ListItem } from 'react-native';
+import store from "../../../api/store.js";
 
-export default function RestoList() {
-  return (
-    <View style={styles.container}>
-      <Text>Restaurant list page</Text>
-    </View>
-  );
+export default class RestoList extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            restaurants: []
+        }
+    }
+
+    render() {
+        if(this.state.restaurants.length === 0){
+            return(
+                <View style={[styles.container, styles.horizontal]}>
+                    <ActivityIndicator size='large' color='#00ff00'/>
+                </View>
+            )
+        }else {
+            return(
+                // <Text>{this.state.restaurants[0].title}</Text>
+                <View style={styles.container}>  
+                    <FlatList
+                        data={this.state.restaurants}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={this.renderItem}
+
+                    />
+                </View>
+            )
+        }
+    }
+
+    renderItem = ({item}) => {
+        return (
+            <Text>
+                {item.title}
+            </Text>
+        )
+    }
+
+
+    componentDidMount() {
+        store.getRestaurants((restaurants) => {
+            this.setState({
+                restaurants: [...restaurants]
+            })
+        })
+    }
+
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      alignItems:'center',
+      justifyContent: 'center'
+    },
+    horizontal: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      padding: 10
+    }
+  })
