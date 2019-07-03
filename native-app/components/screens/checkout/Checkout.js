c2178b8657c43be2229b03343f3122962c2fe451
Anthony <anthony.aoun1@hotmail.com>
2019-07-02 21:00:45 -0400

    improved checkout page

--- native-app/components/screens/checkout/Checkout.js
+++ native-app/components/screens/checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { TextInput } from "react-native-paper";
+import { TextInput, Button } from "react-native-paper";
 
 export default class Checkout extends React.Component {
   state = {
@@ -19,53 +19,64 @@ export default class Checkout extends React.Component {
     const totalPrice = navigation.getParam("totalPrice", "some default value");
     return (
       <View style={styles.container}>
-        <Text>Checkout Page</Text>
-        <TextInput
-          label="Name"
-          value={this.state.name}
-          onChangeText={name => this.setState({ name })}
-        />
-        <TextInput
-          label="City"
-          value={this.state.city}
-          onChangeText={city => this.setState({ city })}
-        />
-        <TextInput
-          label="Address"
-          value={this.state.address}
-          onChangeText={address => this.setState({ address })}
-        />
-        <TextInput
-          label="Postal Code"
-          value={this.state.postal}
-          onChangeText={postal => this.setState({ postal })}
-        />
-        <TextInput
-          label="Card Number"
-          value={this.state.cardNumber}
-          onChangeText={cardNumber => this.setState({ cardNumber })}
-        />
-        <TextInput
-          label="CVC Number"
-          value={this.state.cvcNumber}
-          onChangeText={cvcNumber => this.setState({ cvcNumber })}
-        />
-        <TextInput
-          label="Expiry Date"
-          value={this.state.exp}
-          onChangeText={exp => this.setState({ exp })}
-        />
-        <Text>{totalItems}</Text>
-        <Text>{totalPrice}</Text>
-        <Button
-          disabled={totalItems == 0}
-          mode="contained"
-          color="blue"
-          style={{ borderRadius: 0 }}
-          onPress={() => this.props.navigation.navigate("Home")}
+        <Text style={{ margin: 20, fontSize: 32, fontWeight: '600'}}>Checkout Page</Text>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between"
+          }}
         >
-          Purchase
-        </Button>
+          <TextInput
+            label="Name"
+            value={this.state.name}
+            onChangeText={name => this.setState({ name })}
+          />
+          <TextInput
+            label="City"
+            value={this.state.city}
+            onChangeText={city => this.setState({ city })}
+          />
+          <TextInput
+            label="Address"
+            value={this.state.address}
+            onChangeText={address => this.setState({ address })}
+          />
+          <TextInput
+            label="Postal Code"
+            value={this.state.postal}
+            onChangeText={postal => this.setState({ postal })}
+          />
+          <TextInput
+            label="Card Number"
+            value={this.state.cardNumber}
+            onChangeText={cardNumber => this.setState({ cardNumber })}
+          />
+          <TextInput
+            label="CVC Number"
+            value={this.state.cvcNumber}
+            onChangeText={cvcNumber => this.setState({ cvcNumber })}
+          />
+          <TextInput
+            label="Expiry Date"
+            value={this.state.exp}
+            onChangeText={exp => this.setState({ exp })}
+          />
+          <Text style={{ fontSize: 16, marginLeft: 16 }}>Total Items: {totalItems}</Text>
+          <Text style={{ fontSize: 16, marginLeft: 16 }}>Total Price: ${totalPrice}</Text>
+          <Button
+            disabled={totalItems == 0}
+            mode="contained"
+            color="blue"
+            style={{ borderRadius: 0 }}
+            onPress={() => { 
+              // this.makePurchase();
+              this.props.navigation.navigate("Home")
+            }}
+          >
+            Purchase
+          </Button>
+        </View>
       </View>
     );
   }
@@ -74,8 +85,6 @@ export default class Checkout extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
   }
 });
